c71b311179f6d4dba41b1fa7e18b52a2136d4e29
ericstober <erictstober@gmail.com>
2026-04-02 09:27:56 -0500

    conditional nav links based on logged in status

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 import { Link } from "@tanstack/react-router";
 import { Lightbulb } from "lucide-react";
+import { useAuth } from "@/context/AuthContext";
 
 // Header component (site-wide navigation bar)
 const Header = () => {
+  const { user } = useAuth();
+
   return (
     // Main header container with background and shadow
     <header className='bg-white shadow'>
@@ -26,29 +29,46 @@ const Header = () => {
             Ideas
           </Link>
 
-          {/* Call-to-action button to create a new idea */}
-          <Link
-            to='/ideas/new'
-            className='bg-blue-600 hover:bg-blue-700 text-white font-medium transition px-4 py-2 rounded-md leading-none'
-          >
-            + New Idea
-          </Link>
+          {/* Call-to-action button to create a new idea - only display if logged in */}
+          {user && (
+            <Link
+              to='/ideas/new'
+              className='bg-blue-600 hover:bg-blue-700 text-white font-medium transition px-4 py-2 rounded-md leading-none'
+            >
+              + New Idea
+            </Link>
+          )}
         </nav>
 
         {/* Auth Buttons */}
         <div className='flex items-center space-x-2'>
-          {/* Login */}
-          <Link to='/login' className='text-gray-600 hover:text-gray-700 font-medium transition px-3 py-2 leading-none'>
-            Login
-          </Link>
+          {!user ? (
+            <>
+              {/* Login */}
+              <Link
+                to='/login'
+                className='text-gray-600 hover:text-gray-700 font-medium transition px-3 py-2 leading-none'
+              >
+                Login
+              </Link>
 
-          {/* Register */}
-          <Link
-            to='/register'
-            className='bg-gray-100 hover:bg-gray-200 text-gray-800 font-medium transition px-4 py-2 rounded-md leading-none'
-          >
-            Register
-          </Link>
+              {/* Register */}
+              <Link
+                to='/register'
+                className='bg-gray-100 hover:bg-gray-200 text-gray-800 font-medium transition px-4 py-2 rounded-md leading-none'
+              >
+                Register
+              </Link>
+            </>
+          ) : (
+            <>
+              <span className='text-gray-700 font-medium px-2'>Welcome, {user.name}</span>
+
+              <button className='text-red-600 hover:text-red-900 font-medium transition px-3 py-2 leading-none'>
+                Logout
+              </button>
+            </>
+          )}
         </div>
       </div>
     </header>
